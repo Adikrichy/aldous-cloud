ea9bdbf681788bd8097ff4ac4729082e26645d5e
Adikrichy <adil.erzhanoc.70@gmail.com>
2025-07-14 17:05:10 +0500

    Init the project

---
+++ src/main/java/org/aldouscloud/aldouscloud/AldousCloudApplication.java
@@ -0,0 +1,13 @@
+package org.aldouscloud.aldouscloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AldousCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AldousCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/aldouscloud/aldouscloud/AldousCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aldouscloud.aldouscloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AldousCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
